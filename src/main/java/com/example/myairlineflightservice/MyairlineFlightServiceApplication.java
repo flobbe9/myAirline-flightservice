5790a4c660a92106c9ecef9c165543b122518643
flobbe10 <florin.schikarski@tecfox.eu>
2023-03-01 13:57:26 +0100

    feat: initial commit

---
+++ src/main/java/com/example/myairlineflightservice/MyairlineFlightServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.myairlineflightservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyairlineFlightServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyairlineFlightServiceApplication.class, args);
+	}
+
+}
