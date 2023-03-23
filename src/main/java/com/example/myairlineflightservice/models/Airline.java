2fc2fd5b166cf09c9eff00ef83eb641663f84211
flobbe10 <florin.schikarski@tecfox.eu>
2023-03-23 01:05:44 +0100

    docs: add more dependencies

--- src/main/java/com/example/myairlineflightservice/models/Airline.java
+++ src/main/java/com/example/myairlineflightservice/models/Airline.java
@@ -1,5 +1,6 @@
 package com.example.myAirlineFlightservice.models;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,6 +15,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class Airline {
 
+    @NotBlank
     private String name;
 
 
