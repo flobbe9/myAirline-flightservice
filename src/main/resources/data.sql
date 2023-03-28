976c2df703829358772481cd85f845b4aa1f4df6
flobbe10 <florin.schikarski@tecfox.eu>
2023-03-28 19:29:39 +0200

    docs: add docker-compose, pipeline and data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,27 +1,28 @@
 -- Airline
 INSERT INTO airline(id, name) VALUES(1, 'Ryanair');
 INSERT INTO airline(id, name) VALUES(2, 'Wizz Air');
-INSERT INTO airline(id, name) VALUES(3, 'Tyrkish Airlines');
+INSERT INTO airline(id, name) VALUES(3, 'Turkish Airlines');
 INSERT INTO airline(id, name) VALUES(4, 'Qatar Airways');
 INSERT INTO airline(id, name) VALUES(5, 'Lufthansa');
+INSERT INTO airline(id, name) VALUES(6, 'Eurowings');
 
 
 -- Airport
-INSERT INTO airport(id, name) VALUES(1, 'Berlin airport');
-INSERT INTO airport(id, name) VALUES(2, 'Dortmund airport');
-INSERT INTO airport(id, name) VALUES(3, 'Frankfurt airport');
-INSERT INTO airport(id, name) VALUES(4, 'Hamburg airport');
-INSERT INTO airport(id, name) VALUES(5, 'Hannover airport');
-INSERT INTO airport(id, name) VALUES(6, 'Munich airport');
+INSERT INTO airport(id, name, city_name) VALUES(1, 'Berlin airport', 'Berlin');
+INSERT INTO airport(id, name, city_name) VALUES(2, 'Dortmund airport', 'Dortmund');
+INSERT INTO airport(id, name, city_name) VALUES(3, 'Frankfurt airport', 'Frankfurt');
+INSERT INTO airport(id, name, city_name) VALUES(4, 'Hamburg airport', 'Hamburg');
+INSERT INTO airport(id, name, city_name) VALUES(5, 'Hannover airport', 'Hannover');
+INSERT INTO airport(id, name, city_name) VALUES(6, 'Munich airport', 'Munich');
 
 
 -- City
-INSERT INTO city(id, name) VALUES(1, 'Berlin');
-INSERT INTO city(id, name) VALUES(2, 'Dortmund');
-INSERT INTO city(id, name) VALUES(3, 'Frankfurt');
-INSERT INTO city(id, name) VALUES(4, 'Hamburg');
-INSERT INTO city(id, name) VALUES(5, 'Hannover');
-INSERT INTO city(id, name) VALUES(6, 'Munich');
+INSERT INTO city(id, name, country_name) VALUES(1, 'Berlin', 'Germany');
+INSERT INTO city(id, name, country_name) VALUES(2, 'Dortmund', 'Germany');
+INSERT INTO city(id, name, country_name) VALUES(3, 'Frankfurt', 'Germany');
+INSERT INTO city(id, name, country_name) VALUES(4, 'Hamburg', 'Germany');
+INSERT INTO city(id, name, country_name) VALUES(5, 'Hannover', 'Germany');
+INSERT INTO city(id, name, country_name) VALUES(6, 'Munich', 'Germany');
 
 
 -- Country
