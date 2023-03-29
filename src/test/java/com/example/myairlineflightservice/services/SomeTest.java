9931a66d0f4bfd1d5e05c5ec67029b8b046919fd
flobbe10 <florin.schikarski@tecfox.eu>
2023-03-29 15:30:03 +0200

    the pipe again

---
+++ src/test/java/com/example/myairlineflightservice/services/SomeTest.java
@@ -0,0 +1,17 @@
+package com.example.myairlineflightservice.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.example.myAirlineFlightservice.models.Airline;
+
+
+public class SomeTest {
+    
+    @Test
+    void someTest() {
+
+     
+    }
+}
