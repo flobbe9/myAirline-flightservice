0214cc910b29dcd3cde33b6de27c4cf2580ee50b
flobbe10 <florin.schikarski@tecfox.eu>
2023-03-30 22:39:31 +0200

    refactor: adjust HttpRequestSender

--- src/main/java/com/example/myairlineflightservice/utils/HttpRequestSender.java
+++ src/main/java/com/example/myairlineflightservice/utils/HttpRequestSender.java
@@ -10,19 +10,32 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 
+/**
+ * Helper class with some http request sending methods.
+ * 
+ * @since 0.0.1
+ */
 public class HttpRequestSender {
     
-    // TODO: try this with params and body
-    // TODO: make this more generic
+    /**
+     * Sends a simple http request accepting application/json as content-type. 
+     * <p>
+     * Supports any request method but no request body can be used at the moment.
+     * 
+     * @param url to send the request to.
+     * @param method request method to use.
+     * @return the response as Object.
+     */
     public static Object sendSimpleRequest(String url, HttpMethod method) {
 
         // set headers
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
 
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-        ResponseEntity<?> response = new RestTemplate().exchange(url, method, entity, Object.class);
+        ResponseEntity<?> response = new RestTemplate().exchange(url,   
+                                                                 method, 
+                                                                 new HttpEntity<>(headers), 
+                                                                 Object.class);
 
         return response.getBody();
     }
