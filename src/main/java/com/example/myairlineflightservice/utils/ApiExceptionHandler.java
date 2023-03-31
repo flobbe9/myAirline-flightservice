55a89dd2892ffb27ee439092d521560069c94851
flobbe10 <florin.schikarski@tecfox.eu>
2023-03-31 02:58:02 +0200

    feat: add custom exception

---
+++ src/main/java/com/example/myairlineflightservice/utils/ApiExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.example.myAirlineFlightservice.utils;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+
+/**
+ * Class handling all {@link ApiException}s thrown in RestControllers.
+ * 
+ * @since 0.0.1
+ */
+@ControllerAdvice
+public class ApiExceptionHandler {
+    
+    @ExceptionHandler({ApiException.class})
+    public ResponseEntity<Object> handleApiRequestException(ApiException e) {
+
+        return new ResponseEntity<>(new ApiExceptionWrapper(ZonedDateTime.now(ZoneId.of("Z")),
+                                                            e.getHttpStatus().value(),
+                                                            e.getHttpStatus().getReasonPhrase(),
+                                                            e.getMessage()),
+                                                            e.getHttpStatus());
+    }   
+
+
+    /**
+     * Formats an {@link ApiException}.
+     *
+     * @since 0.0.1
+     */
+    record ApiExceptionWrapper (ZonedDateTime timestamp,
+                                int status,
+                                String error,
+                                String message) { }
+}
